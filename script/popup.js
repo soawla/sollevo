a446ab30c041bf4c94082d8c7d55b32c8a3b7083
soawla <113660685+soawla@users.noreply.github.com>
2022-10-04 16:21:11 +0900

    Delete script directory

--- script/popup.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#event1 .popup1').click(function(){
-        $('.popup').fadeIn();//show()
-    });
-    $('.popup a').click(function(){
-        $('.popup').fadeOut();//hide()
-    });
-});
